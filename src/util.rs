cb52afbd6b4421d3458583d39fd23c369afb5791
Josh Gao <jmgao@google.com>
2019-04-10 12:29:36 -0700

    Move some helpers out to util.

--- src/util.rs
+++ src/util.rs
@@ -62,3 +62,61 @@ pub fn parse_revision<T: AsRef<str>, U: AsRef<str>>(
 
   Ok(object)
 }
+
+pub fn branch_name<'a>(branch: &'a git2::Branch) -> Result<&'a str, Error> {
+  Ok(
+    branch
+      .name()
+      .context("could not determine branch name")?
+      .ok_or_else(|| format_err!("branch name is not valid UTF-8"))?,
+  )
+}
+
+pub fn branch_to_commit<'a>(branch: &'a git2::Branch) -> Result<git2::Commit<'a>, Error> {
+  Ok(
+    branch
+      .get()
+      .peel_to_commit()
+      .context(format_err!("could not resolve {} reference", branch_name(branch)?))?,
+  )
+}
+
+fn find_independent_commits_inner(
+  from: &git2::Commit,
+  to: &git2::Commit,
+  mut accumulator: &mut Vec<git2::Oid>,
+) -> Result<(), Error> {
+  if from.id() == to.id() {
+    return Ok(());
+  }
+
+  accumulator.push(from.id());
+  for parent in from.parents() {
+    find_independent_commits_inner(&parent, to, &mut accumulator)?;
+  }
+  Ok(())
+}
+
+pub fn find_independent_commits(
+  repo: &git2::Repository,
+  src: &git2::Commit,
+  dst: &git2::Commit,
+) -> Result<Vec<git2::Oid>, Error> {
+  let src_id = src.id();
+  let dst_id = dst.id();
+  let common_ancestor = repo.merge_base(src_id, dst_id).context(format_err!(
+    "could not find common ancestor of commits {:?} and {:?}",
+    src,
+    dst
+  ))?;
+
+  let mut accumulator = Vec::new();
+  find_independent_commits_inner(
+    &src,
+    &repo
+      .find_commit(common_ancestor)
+      .context(format_err!("could not find commit matching {}", common_ancestor))?,
+    &mut accumulator,
+  )?;
+  Ok(accumulator)
+}
